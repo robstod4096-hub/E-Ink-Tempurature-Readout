beb873365e8f85eec38348c6e6132e84aa4dd67c
Robert Stoddard <robstod4096@gmail.com>
2026-08-31 17:32:22 -0600

    fixed indentation in bme280_sensor.py

--- bme280_sensor.py
+++ bme280_sensor.py
@@ -14,25 +14,25 @@ calibration_params = bme280.load_calibration_params(bus, address)
 def celsius_to_fahrenheit(celsius):
     return (celsius * 9/5) + 32
 
-def read_sensor_data():0
-            # Read sensor data
-            data = bme280.sample(bus, address, calibration_params)
+def read_sensor_data():
+    # Read sensor data
+    data = bme280.sample(bus, address, calibration_params)
     
-            # Extract temperature, pressure, and humidity
-            temperature_celsius = data.temperature
-            pressure = data.pressure
-            humidity = data.humidity
+    # Extract temperature, pressure, and humidity
+    temperature_celsius = data.temperature
+    pressure = data.pressure
+    humidity = data.humidity
     
-            # Convert temperature to Fahrenheit
-            temperature_fahrenheit = celsius_to_fahrenheit(temperature_celsius)
+    # Convert temperature to Fahrenheit
+    temperature_fahrenheit = celsius_to_fahrenheit(temperature_celsius)
 
-            # Return data as a dictionary
-            return {
-                "temperature_celsius": temperature_celsius,
-                "temperature_fahrenheit": temperature_fahrenheit,
-                "pressure": pressure,
-                "humidity": humidity
-            }
+    # Return data as a dictionary
+    return {
+        "temperature_celsius": temperature_celsius,
+        "temperature_fahrenheit": temperature_fahrenheit,
+        "pressure": pressure,
+        "humidity": humidity
+    }
 
 
 # while True:
