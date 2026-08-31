050c1be2f5c034e93a3c0525b6fdc42b4be122dc
Robert Stoddard <robstod4096@gmail.com>
2026-08-31 17:40:42 -0600

    Bug fix

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'e-Paper/RaspberryPi_Jet
 from waveshare_epd import epd2in13b_V4
 
 epd = epd2in13b_V4.EPD()
+epd.init()
 epd.Clear()
 image = Image.new('1', (epd.width, epd.height), 255)
 draw = ImageDraw.Draw(image)
 
 while True:
         epd.init()
-        draw.text((60, 15), read_sensor_data()[temperature_fahrenheit], font=font, fill=0)
+        draw.text((60, 15), read_sensor_data()['temperature_fahrenheit'], font=font, fill=0)
         epd.sleep()
         time.sleep(60)  # Update every 60 seconds
